5da6091ff7e16cd89ac906b32f9ba07138de7047
huangziking <1475495884@qq.com>
2018-02-23 23:08:09 +0800

    练手1

--- test0/tensorfow1.py
+++ test0/tensorfow1.py
@@ -13,7 +13,7 @@ def create_lexicon(pos_file,neg_file):
     def process_file(f):#读取文件进行分词完成后返回词汇数组
         with open(pos_file,'rb') as f:
             lex=[]
-            lines = f.readline()
+            lines = f.readlines()
             for line in lines:
                 words = nltk.word_tokenize(line.lower())#把所有大写字母转换为小写然后进行分词
                 lex+=words
@@ -99,5 +99,46 @@ def neural_network(data):
     layer_output = tf.add(tf.matmul(layer_2,layer_output_w_b['w_']),layer_output_w_b['b_'])
 
     return layer_output
+
+batch_size = 50
+
+X = tf.placeholder('float',[None,len(train_dataset[0][0])])
+Y = tf.placeholder('float')
+
+def train_neural_network(X,Y):
+    predict = neural_network(X)
+    cost_func = tf.reduce.mean(tf.nn.softmax_cross_entropy_with_logits(predict,Y))
+    optimizer = tf.train.AdamOptimizer().minimize(cost_func)
+
+    epochs = 13
+
+    with tf.Session() as session:
+        session.run(tf.initialize_all_variables())
+        epoch_loss = 0
+
+        i=0
+        random.shuffle(train_dataset)
+        train_x = dataset[:,0]
+        train_y = dataset[:,1]
+
+        for epoch in range(epochs):
+            while i < len(train_x):
+                start = i
+                end = i + batch_size
+                batch_x = train_x[start:end]
+                batch_y = train_y[start:end]
+
+                _,c = session.run([optimizer,cost_func],feed_dict={X:list(batch_x),Y:list(batch_y)})
+
+                epoch_loss += c
+                i += batch_size
+            print(epoch,':',epoch_loss)
+
+        text_x = test_dataset[:,0]
+        text_y = test_dataset[:,1]
+        correct = tf.equal(tf.argmax(predict,1),tf.argmax(Y,1))
+        accuracy = tf.reduce_mean(tf.cast(correct,'float'))
+        print('准确率：',accuracy.eval({X:list(text_x),Y:list(text_y)}))
+train_neural_network(X,Y)
 
 
